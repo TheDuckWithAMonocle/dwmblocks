f8e87994c4ab3a589dae837e9e1957919e677767
TheDuckWithAMonocle <theduckwithamonocle@riseup.net>
2021-07-29 13:53:06 +0530

    Changed the order of a module

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	1,		10},
 
 	{"Vol:", "~/dwmblocks/scripts/volume.sh",				1,		100},
+	
+	{"", "~/dwmblocks/scripts/memory.sh",					1,		10},
 
 	{"", "~/dwmblocks/scripts/battery.sh",					1,		11},
 
